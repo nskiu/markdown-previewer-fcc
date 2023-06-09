213b793e385d7352e59f1fed3843a5ed2f23940b
nskiu <nyelsku@gmail.com>
2023-06-09 08:35:59 +0800

    Add and Style topbar

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,8 +1,20 @@
 const Editor = ({ input, handleChange }) => {
   return (
-    <div>
+    <div
+      className="container d-flex flex-column align-items-center mt-4"
+      id="editordiv"
+    >
+      <div className="topbar d-flex justify-content-between align-items-center rounded-top">
+        <span className="toptitle">
+          <i className="fa fa-heart mx-2"></i>
+          Editor
+        </span>
+        <button className="btn ">
+          <i className="fa fa-arrows-alt"></i>
+        </button>
+      </div>
       <textarea
-        className="rounded"
+        className=" p-1 mx-auto"
         id="editor"
         onChange={(event) => handleChange(event.target.value)}
         value={input}
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,5 +1,18 @@
 const Preview = () => {
-  return <div id="preview"></div>;
+  return (
+    <div>
+      <div className="topbar d-flex justify-content-between align-items-center rounded-top">
+        <span className="toptitle">
+          <i className="fa fa-heart mx-2"></i>
+          Preview
+        </span>
+        <button className="btn">
+          <i className="fa fa-arrows-alt"></i>
+        </button>
+      </div>
+      <div id="preview"></div>
+    </div>
+  );
 };
 
 export default Preview;
